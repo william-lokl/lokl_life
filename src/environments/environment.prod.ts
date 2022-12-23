baadf38eddc524db55770dbd11ec3e443c88e250
JuanRosero1907 <juanjose@fabioarias.co>
2022-12-23 15:02:36 -0500

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  paypalUrl:
+    'https://www.paypal.com/sdk/js?client-id=AeA_0Gnh8IxWgv0UeP4kOYbLb1T_ETs7ticZvx1DL-mQGOMiUysp0gyQmEPWcrpxHG_r5oTrqLIdln2J',
+  athUrl: 'https://www.athmovil.com/api/js/v3/athmovilV3.js',
+  url: 'https://apitest.pagatodopr.com/v2/api',
+  clientPaypal:
+    'AeA_0Gnh8IxWgv0UeP4kOYbLb1T_ETs7ticZvx1DL-mQGOMiUysp0gyQmEPWcrpxHG_r5oTrqLIdln2J',
+  porcentaje: 0.03,
 };
