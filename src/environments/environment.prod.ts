aa6a4eeeb0dd161fcdbe3390e49dcd0d12fc328f
kevin-tooles <kevinazul999@gmail.com>
2023-07-10 17:37:07 -0500

    ajuste de enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-  paypalUrl: 'https://www.paypal.com/sdk/js?client-id=AaVhJUWehYHlZ7iupmf9p_U2v-zNEKabiV6dISj4oWqlFJBtGie5yTjvk7Lui90ddN5R50pt7Zcn5_I9',
-  athUrl: 'https://www.athmovil.com/api/js/v3/athmovilV3.js',
-  url: 'https://api.pagatodopr.com/v2/api',
-  clientPaypal: 'AeA_0Gnh8IxWgv0UeP4kOYbLb1T_ETs7ticZvx1DL-mQGOMiUysp0gyQmEPWcrpxHG_r5oTrqLIdln2J',
-  porcentaje: 0.03,
+
 };
