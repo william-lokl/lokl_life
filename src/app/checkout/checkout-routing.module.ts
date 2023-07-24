64ad96844807d99015a157d975e2c7e299103290
Juan Camilo <juanksr0@gmail.com>
2023-07-24 11:10:05 -0500

    organización de modulos y rutas

--- src/app/checkout/checkout-routing.module.ts
+++ src/app/checkout/checkout-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InversionComponent } from './pages/inversion/inversion.component';
+import { AutentificacionComponent } from './pages/autentificacion/autentificacion.component';
 
 const routes: Routes = [
   {path: 'invest', component: InversionComponent},
-  {path: 'personal-data', component: InversionComponent},
+  {path: 'personal-data', component: AutentificacionComponent},
 ];
 
 @NgModule({
